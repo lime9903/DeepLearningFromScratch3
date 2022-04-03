795fe3753c451cbd253b8a6981395323d743ecd0
lime9903 <lime9903@daum.net>
2022-04-03 23:57:46 +0900

    main에서 커밋

---
+++ steps/step01.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+# data = np.array(1.0)
+# x = Variable(data)
+# print(x.data)
+
+# x.data = np.array(2.0)
+# print(x.data)
+
+# x = np.array(1)
+# print(x.ndim)
+
+# y = np.array([1, 2, 3])
+# print(y.ndim)
+
+# z = np.array([[1, 2, 3],
+#               [4, 5, 6],
+#               [7, 8, 9]])
+# print(z.ndim)
---
+++ steps/step04.py
@@ -0,0 +1,26 @@
+from step01 import *
+from step02 import *
+from step03 import *
+import numpy as np
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+# f = Square()
+# x = Variable(np.array(2.0))
+# dy = numerical_diff(f, x)
+# print(dy)
+
+# def f(x):
+#     A = Square()
+#     B = Exp()
+#     C = Square() 
+#     return C(B(A(x)))
+
+# x = Variable(np.array(0.5))
+# dy = numerical_diff(f, x)
+# print(dy)
